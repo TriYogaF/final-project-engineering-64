fc8c4da34e546849135052dbecd07b740c3b0e75
Rachmat Japasima <rachmat.japasima7@gmail.com>
2022-06-13 23:21:27 +0700

    feat: add login-api endpoint

--- Backend/handler/user.go
+++ Backend/handler/user.go
@@ -46,3 +46,42 @@ func (h *userHandler) RegisterUser(c *gin.Context){
 
 	c.JSON(http.StatusOK, response)
 }	
+
+
+func (h *userHandler) Login(c *gin.Context) {
+	// user menginput data
+	// inputan user diambil handler
+	// mapping input user ke input struct
+	// input struct kirim ke service
+	// service mencari data user dengan bantuan repository
+	// match password
+
+
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Login Failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		 c.JSON(http.StatusUnprocessableEntity, response)
+		 return
+	}
+
+	loggedinUser, err := h.userService.Login(input)
+
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helper.APIResponse("Login Failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		 c.JSON(http.StatusUnprocessableEntity, response)
+		 return
+	}
+
+	formatter := user.FormatUser(loggedinUser, "token123456789")
+
+	response := helper.APIResponse("Login Successfully", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
