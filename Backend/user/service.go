7da7d1b94b6b733a3c6e21fb925b10543434fe54
Rachmat Japasima <rachmat.japasima7@gmail.com>
2022-06-14 22:57:17 +0700

    feat: add upload avatar endpoint

--- Backend/user/service.go
+++ Backend/user/service.go
@@ -10,6 +10,7 @@ type Service interface {
 	RegisterUser(input RegisterUserInput) (User, error)
 	Login(input LoginInput) (User, error)
 	IsEmailAvailable(input CheckEmailInput) (bool, error)
+	SaveAvatar(ID int, fileLocation string) (User, error)
 }
 
 type service struct {
@@ -82,6 +83,26 @@ func (s *service) IsEmailAvailable(input CheckEmailInput) (bool, error){
 	return false, nil
 
 }
+
+	func (s *service) SaveAvatar(ID int, fileLocation string) (User, error){
+		// find user by id
+		user, err := s.repository.FindByID(ID)
+		if err != nil {
+			return user, err
+		}
+
+		// update atribute file_avatar 
+		user.FileAvatar = fileLocation
+
+		// save field file_avatar
+		updateUser, err := s.repository.Update(user)
+		if err != nil {
+			return updateUser, err
+		}
+
+		return updateUser, nil
+	}
+
 
 // mapping struct input ke strcut user
 // simpan struct User melalui repository
