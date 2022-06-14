aa06e89f61ae447629374bb43e4191f1bbbc19ca
TriYogaF <triyogaf11@gmail.com>
2022-06-14 12:26:30 +0700

    add layout upload-book

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import UploadBook from "./pages/UploadBook";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
+      <Routes>
+        <Route path="/uploadbook" element={<UploadBook />} />
+      </Routes>
     </div>
   );
 }
