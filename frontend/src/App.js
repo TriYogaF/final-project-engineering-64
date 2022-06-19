b0ae2de537f006835939b5916a91968179b1b7db
TriYogaF <triyogaf11@gmail.com>
2022-06-19 15:29:55 +0700

    add layout description-book

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import Navbar from "./component/NavBar";
-import Header from "./component/Header";
 import Content from "./pages/Content";
 
 import UploadBook from "./pages/UploadBook";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import { Route, Routes } from "react-router-dom";
+import DescriptionBook from "./pages/DescriptionBook";
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
           <Route path="/uploadbook" element={<UploadBook />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/description" element={<DescriptionBook />} />
         </Routes>
       </div>
     </main>
