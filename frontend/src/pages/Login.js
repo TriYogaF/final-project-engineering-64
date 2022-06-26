d8e482a9fc7d6174295070cbccc6bfd232d102f4
TriYogaF <triyogaf11@gmail.com>
2022-06-26 18:20:47 +0700

    add protected route

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,7 @@
-import { useState, useEffect, useRef, useContext } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Container, Form, Row, Col, Button } from "react-bootstrap";
-import AuthContext from "../context/AuthProvider";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 import axios from "../api/axios";
 
 const sizeImg = {
@@ -11,14 +12,18 @@ const sizeImg = {
 const LOGIN_URL = "/sessions";
 
 export default function Login() {
-  const { setAuth } = useContext(AuthContext);
+  const { setAuth } = useAuth();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
   const emailRef = useRef();
   const errRef = useRef();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errMsg, setErrMsg] = useState("");
-  const [success, setSuccess] = useState(false);
 
   useEffect(() => {
     emailRef.current.focus();
@@ -37,13 +42,15 @@ export default function Login() {
       });
       // console.log(JSON.stringify(response?.data));
       // console.log(response?.data);
-      // console.log(response?.data.data.token);
       const accessToken = response?.data?.data?.token;
-      const roles = response?.data?.roles;
+      const roles = response?.data?.data?.role;
+      console.log(accessToken);
+      console.log(roles);
+      console.log(from);
       setAuth({ email, password, roles, accessToken });
       setEmail("");
       setPassword("");
-      setSuccess(true);
+      navigate(from, { replace: true });
     } catch (err) {
       if (!err?.response) {
         setErrMsg("No Server Response");
@@ -59,63 +66,50 @@ export default function Login() {
   };
 
   return (
-    <>
-      {" "}
-      {success ? (
-        <section>
-          <h1>You are logged in!</h1>
-          <br />
-          <p>
-            <a href="/">Go to Home</a>
-          </p>
-        </section>
-      ) : (
-        <section className="gh-section">
-          <Container className="pb-5">
-            <Row className="align-items-center">
-              <Col className="col-7">
-                <h1 className="p-5 text-light">
-                  <strong>Membaca</strong> memperkaya ilmuku dan memperluas imajinasiku
-                </h1>
-                <img src="../assets/img/book.png" style={sizeImg} className="float-end"></img>
-              </Col>
-              <Col className="bg-light m-5 rounded-2">
-                <Container className="p-4 mt-2 mb-5">
-                  <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">
-                    {errMsg}
-                  </p>
-                  <div className="rounded-2 py-2 mb-5 gh-bg-primary">
-                    <h2 className="text-center text-light">Diary</h2>
-                  </div>
-                  <Form onSubmit={handleSubmit} className="d-grid">
-                    <Form.Group className="mb-3" controlId="inputEmail">
-                      <Form.Label>Email address</Form.Label>
-                      <Form.Control type="email" placeholder="Enter email" ref={emailRef} autoComplete="off" onChange={(e) => setEmail(e.target.value)} value={email} required />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="InputPassword">
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} required />
-                    </Form.Group>
-                    <Button variant="light" className="text-light gh-bg-primary" type="submit">
-                      Login
-                    </Button>
-                  </Form>
-                  <div className="text-center pt-4">
-                    <p>
-                      If you don't have an account
-                      <br />
-                      You can{" "}
-                      <a href="/register" className="link-dark gh-regist">
-                        Register here !
-                      </a>
-                    </p>
-                  </div>
-                </Container>
-              </Col>
-            </Row>
-          </Container>
-        </section>
-      )}
-    </>
+    <section className="gh-section">
+      <Container className="pb-5">
+        <Row className="align-items-center">
+          <Col className="col-7">
+            <h1 className="p-5 text-light">
+              <strong>Membaca</strong> memperkaya ilmuku dan memperluas imajinasiku
+            </h1>
+            <img src="../assets/img/book.png" alt="logo" style={sizeImg} className="float-end"></img>
+          </Col>
+          <Col className="bg-light m-5 rounded-2">
+            <Container className="p-4 mt-2 mb-5">
+              <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">
+                {errMsg}
+              </p>
+              <div className="rounded-2 py-2 mb-5 gh-bg-primary">
+                <h2 className="text-center text-light">Diary</h2>
+              </div>
+              <Form onSubmit={handleSubmit} className="d-grid">
+                <Form.Group className="mb-3" controlId="inputEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" ref={emailRef} onChange={(e) => setEmail(e.target.value)} value={email} required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="InputPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} required />
+                </Form.Group>
+                <Button variant="light" className="text-light gh-bg-primary" type="submit">
+                  Login
+                </Button>
+              </Form>
+              <div className="text-center pt-4">
+                <p>
+                  If you don't have an account
+                  <br />
+                  You can{" "}
+                  <Link to="/register" className="link-dark gh-regist text-decoration-underline">
+                    Register here !
+                  </Link>
+                </p>
+              </div>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   );
 }
--- frontend/src/pages/ReadingSite.js
+++ frontend/src/pages/ReadingSite.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import PDFViewer from "pdf-viewer-reactjs";
 
 export default function ReadingSite() {
@@ -8,7 +8,7 @@ export default function ReadingSite() {
       <Container className="py-5 border">
         <PDFViewer
           document={{
-            url: "https://arxiv.org/pdf/quant-ph/0410100.pdf",
+            url: "https://www.ets.org/Media/Tests/GRE/pdf/gre_research_validity_data.pdf",
           }}
           scale={1.3}
         />
